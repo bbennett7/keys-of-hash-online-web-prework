38d89b9f89068dbd9e184dc34bf62c5ef2fd220b
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-03 20:21:44 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      arguments.each do |item|
+        if item == value
+          array << key
+        end
+      end
+    end
+    array
   end
-end
+end 
